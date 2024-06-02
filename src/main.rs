68bda007c2066cc8329c3fc39ef3747a3e9ccc0e
Dennis Mellert <dennis.mellert@otto.de>
2024-06-02 12:45:30 +0200

    improved logging

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::fs;
 use std::path::PathBuf;
 use std::time::Duration;
 
-use axum::{Extension, middleware};
 use axum::extract::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::{middleware, Extension};
 use clap::Parser;
+use env_logger::Builder;
 use rand::Rng;
+use std::io::Write;
 
 use crate::routing::config::Config;
 use crate::routing::options::Options;
@@ -19,9 +21,9 @@ mod routing;
 #[command(name = "fauxd", about = "Serve mock data")]
 pub struct Cli {
     #[arg(long)]
-    pub port: u16,
+    pub port: Option<u16>,
     #[arg(long)]
-    pub host: String,
+    pub host: Option<String>,
     #[arg(help = "The config file describing the routes")]
     pub config: PathBuf,
 }
@@ -44,15 +46,36 @@ async fn delay_response(
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    let mut builder = Builder::new();
+    builder
+        .format(|buf, rec| writeln!(buf, "{}", rec.args()))
+        .filter(None, log::LevelFilter::Debug)
+        .write_style(env_logger::WriteStyle::Always)
+        .init();
     let args = Cli::parse();
     let config = fs::read_to_string(args.config).expect("Unable to read config");
     let config: Config = serde_yaml::from_str(&config).unwrap();
 
-    let options = config.options.clone().unwrap_or(Options {
-        min_response_delay_ms: None,
-        max_response_delay_ms: None,
-    });
+    let mut options = config.options.clone().unwrap_or_default();
+    if args.host.is_some() {
+        log::info!("Setting host to: {:?}", args.host);
+        options.host = args.host;
+    }
+
+    if options.host.is_none() {
+        eprintln!("'host' must either be set in the config or via the command line");
+        std::process::exit(1);
+    }
+
+    if args.port.is_some() {
+        log::info!("Setting port to: {:?}", args.port);
+        options.port = args.port;
+    }
+
+    if options.port.is_none() {
+        eprintln!("'port' must either be set in the config or via the command line");
+        std::process::exit(1);
+    }
 
     let router = config
         .router()
@@ -60,10 +83,33 @@ async fn main() {
             options.clone(),
             delay_response,
         ))
-        .layer(Extension(options));
+        .layer(Extension(options.clone()));
 
-    let listener = tokio::net::TcpListener::bind(format!("{}:{}", args.host, args.port))
-        .await
-        .unwrap();
-    axum::serve(listener, router).await.unwrap();
+    match tokio::net::TcpListener::bind(format!(
+        "{}:{}",
+        options.clone().host.unwrap(),
+        options.clone().port.unwrap()
+    ))
+    .await
+    {
+        Ok(listener) => {
+            log::info!(
+                "Starting server on: {}:{}",
+                options.host.unwrap(),
+                options.port.unwrap()
+            );
+
+            if let Err(e) = axum::serve(listener, router).await {
+                log::error!("Failed to start server - {e}");
+            }
+        }
+        Err(e) => {
+            log::error!(
+                "Failed to bind to: {}:{} - {e}",
+                options.host.unwrap(),
+                options.port.unwrap()
+            );
+            std::process::exit(1);
+        }
+    }
 }
--- src/routing/options.rs
+++ src/routing/options.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct Options {
+    pub host: Option<String>,
+    pub port: Option<u16>,
     pub min_response_delay_ms: Option<u64>,
     pub max_response_delay_ms: Option<u64>,
 }
